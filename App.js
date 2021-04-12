dc90e08c54ea230f8690a7542346f03d705fa7ea
janellebaguley <nellie.baguley@gmail.com>
2021-04-12 10:17:06 -0600

    updates to components

--- App.js
+++ App.js
@@ -1,6 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {NavigationCenter} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+import HomeScreen from './src/screens/HomeScreen';
+import DetailsScreen from './src/screens/DetailsScreen';
+import ImageScreen from './src/screens/ImageScreen';
 
 export default function App() {
   return (
@@ -19,3 +25,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
